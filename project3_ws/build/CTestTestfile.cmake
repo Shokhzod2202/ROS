408d080d56774a2599b338a20505d180de4f8fbd
SmartMobSer <shaxzodxaydarov257@gmail.com>
2023-03-15 14:49:54 +0900

    Initial Commit

---
+++ project3_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/shokhzod/catkin_ws/project3_ws/src
+# Build directory: /home/shokhzod/catkin_ws/project3_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("project3")
---
+++ project3_ws/build/project3/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/shokhzod/catkin_ws/project3_ws/src/project3
+# Build directory: /home/shokhzod/catkin_ws/project3_ws/build/project3
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
